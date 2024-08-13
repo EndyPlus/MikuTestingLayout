9c77f202fbf46533249dc66846dcf1a652a2e459
Artem <aapliusnin@gmail.com>
2024-08-13 21:35:40 +0300

    fix burger + improved

--- burger.js
+++ burger.js
@@ -1,28 +1,46 @@
 let icon = document.getElementById("burgerBtn");
 let burgerList = document.getElementsByClassName("burger-list");
 let burgerListUl = document.querySelector("ul");
-let burgerActive = false;
+
+const activeBurgerMenu = localStorage.getItem("status");
+
+function enableBurgerMenu() {
+  icon.src = "/assets/close_icon.png";
+  burgerListUl.style.display = "flex";
+  localStorage.setItem("status", "active");
+}
+
+function disableBurgerMenu() {
+  icon.src = "/assets/hamburger_icon.png";
+  burgerListUl.style.display = "none";
+  localStorage.removeItem("status");
+}
 
 function burger() {
-  if (burgerActive == false) {
-    icon.src = "/assets/close_icon.png";
-    burgerListUl.style.display = "flex";
-    burgerActive = true;
+  if (!activeBurgerMenu) {
+    enableBurgerMenu();
   } else {
-    icon.src = "/assets/hamburger_icon.png";
-    burgerListUl.style.display = "none";
-    burgerActive = false;
+    disableBurgerMenu();
   }
 }
 
 // checking window size
 function checkWindowSize() {
-  if (window.innerWidth >= 730) {
+  if (window.innerWidth > 730) {
     icon.src = "/assets/hamburger_icon.png";
     burgerListUl.style.display = "none";
     burgerActive = false;
   }
 }
 
+function checkMobileReload() {
+  if (window.innerWidth <= 730 && activeBurgerMenu == "active") {
+    enableBurgerMenu();
+  }
+}
+
 // following size changing
 window.addEventListener("resize", checkWindowSize);
+
+// following page reloading
+window.addEventListener("load", checkMobileReload);
