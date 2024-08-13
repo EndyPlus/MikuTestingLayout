b7a25c07f8461b3cb2a95775acc86e685d23ca9f
Artem <aapliusnin@gmail.com>
2024-08-13 20:39:25 +0300

    working burger menu

--- burger.js
+++ burger.js
@@ -1,3 +1,28 @@
+let icon = document.getElementById("burgerBtn");
+let burgerList = document.getElementsByClassName("burger-list");
+let burgerListUl = document.querySelector("ul");
+let burgerActive = false;
+
 function burger() {
-  console.log("clicked");
+  if (burgerActive == false) {
+    icon.src = "/assets/close_icon.png";
+    burgerListUl.style.display = "flex";
+    burgerActive = true;
+  } else {
+    icon.src = "/assets/hamburger_icon.png";
+    burgerListUl.style.display = "none";
+    burgerActive = false;
+  }
 }
+
+// checking window size
+function checkWindowSize() {
+  if (window.innerWidth >= 730) {
+    icon.src = "/assets/hamburger_icon.png";
+    burgerListUl.style.display = "none";
+    burgerActive = false;
+  }
+}
+
+// following size changing
+window.addEventListener("resize", checkWindowSize);
